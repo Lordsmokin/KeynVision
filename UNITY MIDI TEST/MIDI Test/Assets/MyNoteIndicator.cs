1ffc2b65af3707731859c122411bd9b8100fa98d
TAMATEBAKO-BOX\amare <amarezport@gmail.com>
2017-05-21 11:08:21 -0400

    First MIDI commit. Middle-C octave is being used to destroy incoming notes but the prefabs are not being destroyed after first row of notes pass.

---
+++ UNITY MIDI TEST/MIDI Test/Assets/MyNoteIndicator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using MidiJack;
+
+public class MyNoteIndicator : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public int noteNumber;
+
+	// Update is called once per frame
+	void Update () {
+		Rigidbody r = GetComponent<Rigidbody> ();
+		r.velocity = Vector3.forward * speed;
+
+		//transform.localScale = Vector3.one * (1f + MidiMaster.GetKey(noteNumber));
+
+	}
+}
---
+++ UNITY MIDI TEST/MIDI Test/Assets/simpleZmover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class simpleZmover : MonoBehaviour {
+
+	public GameObject noteClone;
+	public float speed;
+	public float destroyMissedNote;
+	public float hitZone;
+	public Transform keyFlyDirection;
+	public int noteNumber;
+	private GameObject keyboard;
+
+
+	void Start () {
+		
+		keyboard = GameObject.FindGameObjectWithTag ("keyboard");
+
+		Rigidbody r = GetComponent<Rigidbody> ();
+		r.velocity = keyboard.transform.forward.normalized * speed * -1f;
+		Vector3 targetRotation = Vector3.zero;
+		targetRotation.x = keyboard.transform.rotation.eulerAngles.z;
+		targetRotation.z = keyboard.transform.rotation.eulerAngles.x;
+		transform.rotation = keyboard.transform.rotation;
+
+	}
+
+	void Update () {
+
+		if (noteClone.transform.position.z <= hitZone && MidiMaster.GetKeyDown (noteNumber)) {
+			Destroy (noteClone.gameObject);
+		} else if (noteClone.transform.position.z <= hitZone && MidiMaster.GetKeyDown (noteNumber)) {
+			Destroy (noteClone.gameObject);
+//			(noteClone.transform.position.z <= destroyMissedNote) {
+//			Destroy (gameObject);
+		}
+
+
+	}
+
+}
