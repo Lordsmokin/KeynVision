7e76f5ba2bc65c915a5120c568cb24e96b98a6c1
TAMATEBAKO-BOX\amare <amarezport@gmail.com>
2017-05-31 17:57:52 -0400

    Simpler spawner logic.

--- KeynVision_Prototype/Assets/MIDI/Scripts/LocalNoteSpawner.cs
+++ KeynVision_Prototype/Assets/MIDI/Scripts/LocalNoteSpawner.cs
@@ -5,72 +5,18 @@ using UnityEngine;
 public class LocalNoteSpawner : MonoBehaviour {
 
 	public GameObject whiteNotes;
-	private GameObject keyboard;
-
-	public float startDistance;
-	//public bool lessThan = true;
-
-	//public float[] arrayNotes;
-	public Transform keySpawnPosition;
 	public Transform keyFlyDirection;
+	public float spawnInterval = 8f;
+	public float timeToSpawn = 0f;
 
-	public float spawnTime;
-	private float timeToSpawn;
-
-	/*
-	// Use this for initialization
-	IEnumerator Start () {
-
-		timeToSpawn = 0f;
-
-		keyboard = GameObject.FindGameObjectWithTag ("keyboard");
-
-		// alternative coroutine version:
-		// start a coroutine but with a delay of timetospawn
-		// the coroutine itself only does the for loop
-		// at the end of the coroutine it reschedules iself with a delay
-		yield return StartCoroutine(SpawnNotes(0f));
-	}
-
-
-	IEnumerator SpawnNotes(float delay) {
-		yield return WaitForSeconds (delay);
-		for (int index = 0; index <= 2; index++) 
-		{
-			GameObject noteModel = whiteNotes;
-			float upOffset = 0.02f;
-			Vector3 spawnPosition = new Vector3 (keySpawnPosition.position.x, keySpawnPosition.position.y, keySpawnPosition.position.z);
-			spawnPosition += keyboard.transform.up.normalized * upOffset;
-			spawnPosition += keyboard.transform.forward.normalized * startDistance;
-			Instantiate (noteModel, spawnPosition, Quaternion.identity); //, gameObject.transform);
-		}
-		yield return StartCoroutine(SpawnNotes(spawnTime));
-	}
-	*/
-
-	void start() {
-		timeToSpawn = 0f;
-	}
-
-	// Update is called once per frame
 	void Update () {
-
 		timeToSpawn -= Time.deltaTime;
-
 		if (timeToSpawn <= 0f) {
-
-				GameObject noteModel = whiteNotes;
-				keyboard = GameObject.FindGameObjectWithTag ("keyboard");
-
-				float upOffset = 0.02f;
-
-				Vector3 spawnPosition = new Vector3 (keySpawnPosition.position.x, keySpawnPosition.position.y, keySpawnPosition.position.z);
-				spawnPosition += keyboard.transform.up.normalized * upOffset;
-				spawnPosition += keyboard.transform.forward.normalized * startDistance;
-			GameObject instantiatedNote = Instantiate (noteModel, spawnPosition, Quaternion.identity) as GameObject;//, gameObject.transform);
+			GameObject instantiatedNote = Instantiate (whiteNotes, transform.position, Quaternion.identity) as GameObject;
 			instantiatedNote.GetComponent<simpleZmover>().keyFlyDirection = keyFlyDirection;
-
-			timeToSpawn = spawnTime;
+			if (spawnInterval > 0f) {
+				timeToSpawn = spawnInterval;
+			}
 		}
 	}
 		
---
+++ KeynVision_Prototype/Assets/SongSetup.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongSetup : MonoBehaviour {
+
+	public GameObject FlyDirections;
+
+	public GameObject CSpawner;
+	public GameObject DSpawner;
+	public GameObject ESpawner;
+	public GameObject FSpawner;
+	public GameObject GSpawner;
+	public GameObject ASpawner;
+	public GameObject BSpawner;
+	public GameObject C1Spawner;
+
+	void Start () {
+		// Create the octave spawners:
+		//CreateOctave();
+		CreateOther();
+	}
+
+	void CreateOctave() {
+		CreateNote ("C", 0f, 8f);
+		CreateNote ("D", 1f, 8f);
+		CreateNote ("E", 2f, 8f);
+		CreateNote ("F", 3f, 8f);
+		CreateNote ("G", 4f, 8f);
+		CreateNote ("A", 5f, 8f);
+		CreateNote ("B", 6f, 8f);
+		CreateNote ("C1", 7f, 8f);
+	}
+
+	void CreateOther() {
+		CreateNote ("C", 0f, 8f);
+		CreateNote ("E", 1f, 8f);
+		CreateNote ("G", 2f, 8f);
+		CreateNote ("E", 3f, 8f);
+		CreateNote ("C", 4f, 8f);
+		CreateNote ("B", 5f, 8f);
+		CreateNote ("A", 6f, 8f);
+		CreateNote ("C", 7f, 8f);
+	}
+
+	void CreateNote(string note, float timeToStart, float interval) {
+		GameObject spawnerPrefab = GetSpawner (note);
+		Transform spawnParent = GetSpawnParent (note);
+		Transform target = GetNoteTarget (note);
+		// now instantiate the new spawner:
+		GameObject newSpawner = Instantiate(spawnerPrefab, spawnParent, false);
+		newSpawner.GetComponent<LocalNoteSpawner> ().timeToSpawn = timeToStart;
+		newSpawner.GetComponent<LocalNoteSpawner> ().spawnInterval = interval;
+		newSpawner.GetComponent<LocalNoteSpawner> ().keyFlyDirection = target;
+	}
+
+	GameObject GetSpawner(string note) {
+		if (note == "C") {
+			return CSpawner;
+		} else if (note == "D") {
+			return DSpawner;
+		} else if (note == "E") {
+			return ESpawner;
+		} else if (note == "F") {
+			return FSpawner;
+		} else if (note == "G") {
+			return GSpawner;
+		} else if (note == "A") {
+			return ASpawner;
+		} else if (note == "B") {
+			return BSpawner;
+		} else if (note == "C1") {
+			return C1Spawner;
+		}
+		return null;
+	}
+
+	Transform GetSpawnParent(string note) {
+		return transform.FindChild (note).transform;
+	}
+
+	Transform GetNoteTarget(string note) {
+		return FlyDirections.transform.FindChild (note + "-FlyDirection").transform;
+	}
+}
